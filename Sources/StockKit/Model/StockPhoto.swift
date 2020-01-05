559da041e047c4c37c8d2d6e43c27d4d662ae024
Ayden <aydenp@users.noreply.github.com>
2020-01-04 19:55:25 -0500

    Split into more files for clarity

--- Sources/StockKit/Model/StockPhoto.swift
+++ Sources/StockKit/Model/StockPhoto.swift
@@ -12,14 +12,3 @@ public struct StockPhoto: Codable, Hashable {
     public let thumbnailURL: URL?, fullImageURL: URL
     public let alternativeText: String?
 }
-
-public struct StockPhotoResults {
-    /// The photos retrieved as part of this request.
-    public let photos: [StockPhoto]
-    /**
-     A token that can be used to provide information required to retrieve the next page, such as the last photo's ID or the offset from the previous request.
-     
-     If not provided, pagination cannot be performed, such as if a service does not perform pagination or has reached the end of the returnable photos.
-     */
-    public let paginationToken: String?
-}
--- Sources/StockKit/Model/StockPhotoService.swift
+++ Sources/StockKit/Model/StockPhotoService.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-public typealias StockPhotoSearchCompletionBlock = (Result<StockPhotoResults, Error>) -> Void
-
 public protocol StockPhotoService: class {
     /// An internal identifier for this service.
     static var identifier: String { get }
@@ -29,75 +27,4 @@ public protocol StockPhotoService: class {
     func search(with query: String, count: Int, after paginationToken: String?, completion: @escaping StockPhotoSearchCompletionBlock) -> StockPhotoSearchRequest?
 }
 
-/// Optional methods on the search request being made, such as cancellation.
-public protocol StockPhotoSearchRequest {
-    /// A method which will cancel the running request, if necessary.
-    func cancel()
-}
-
-/// A pre-made search request that allows you to create a custom completion handler representing a network search's data task.
-public class StockPhotoSearchNetworkRequest: StockPhotoSearchRequest {
-    let dataTask: URLSessionDataTask
-    
-    init(dataTask: URLSessionDataTask) {
-        self.dataTask = dataTask
-        dataTask.resume()
-    }
-    
-    convenience init(urlRequest: URLRequest, completion: @escaping (Result<Data, Error>, URLResponse?) -> Void) {
-        self.init(dataTask: URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            guard let data = data, error == nil else { completion(.failure(error ?? StockPhotoSearchError.noData), response); return }
-            completion(.success(data), response)
-        })
-    }
-    
-    convenience init(url: URL, completion: @escaping (Result<Data, Error>, URLResponse?) -> Void) {
-        self.init(urlRequest: URLRequest(url: url), completion: completion)
-    }
-    
-    convenience init<T: Decodable>(jsonType: T.Type, urlRequest: URLRequest, completion: @escaping (Result<T, Error>, URLResponse?) -> Void) {
-        self.init(urlRequest: urlRequest, completion: { (result, response) in
-            do {
-                let data = try result.get()
-                let decoded = try JSONDecoder().decode(T.self, from: data)
-                completion(.success(decoded), response)
-            } catch let error {
-                completion(.failure(error), response)
-            }
-        })
-    }
-
-    convenience init<T: Decodable>(jsonType: T.Type,url: URL, completion: @escaping (Result<T, Error>, URLResponse?) -> Void) {
-        self.init(jsonType: jsonType, urlRequest: URLRequest(url: url), completion: completion)
-    }
-    
-    convenience init<T: Decodable & StockPhotoResultsProviding>(jsonResultsProvidingType: T.Type, urlRequest: URLRequest, completion: @escaping (Result<StockPhotoResults, Error>) -> Void) {
-        self.init(jsonType: T.self, urlRequest: urlRequest, completion: { (result, response) in
-            switch result {
-            case .success(let response): completion(.success(response.results))
-            case .failure(let error): completion(.failure(error))
-            }
-        })
-    }
-    
-    convenience init<T: Decodable & StockPhotoResultsProviding>(jsonResultsProvidingType: T.Type,url: URL, completion: @escaping (Result<StockPhotoResults, Error>) -> Void) {
-        self.init(jsonResultsProvidingType: jsonResultsProvidingType, urlRequest: URLRequest(url: url), completion: completion)
-    }
-    
-    public func cancel() {
-        dataTask.cancel()
-    }
-}
-
-public protocol StockPhotoResultsProviding {
-    var results: StockPhotoResults { get }
-}
-
-public enum StockPhotoSearchError: Error {
-    /// No data was retrieved during the search, but there is no more approprate error to display.
-    case noData
-    /// An unknown error occurred during deserialization.
-    case deserializationError
-    /// A URL could not be formed to create a network request.
-    case noValidURL
-}
+public typealias StockPhotoSearchCompletionBlock = (Result<StockPhotoResults, Error>) -> Void
